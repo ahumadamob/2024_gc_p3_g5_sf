1743de7626ae2759c620586248ff4f68ac95d60e
Ivan <balmacedaivan5@gmail.com>
2024-05-09 21:12:36 -0300

    Eliminar anotacion Table y Column

--- src/main/java/com/iesmb/gestionalumnos/Entity/Curso.java
+++ src/main/java/com/iesmb/gestionalumnos/Entity/Curso.java
@@ -3,43 +3,20 @@ package com.iesmb.gestionalumnos.Entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="curso")
 public class Curso {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    /*@ManyToOne
-    //@JoinColumn(name = "id_materia")
-    private Materia materia;------------ A RELACIONAR ----------*/
-    @Column(name = "materia")
     private String materia;
-
-    /*@ManyToOne
-    @JoinColumn(name = "id_profesor")
-    private Profesor profesor;------------ A RELACIONAR ----------*/
-    @Column(name = "profesor")
     private String profesor;
-
-    @Column(name = "anio_academico")
     private int anioAcademico;
-
-    @Column(name = "semestre")
     private String semestre;
-
-    @Column(name = "horario")
     private String horario;
-
-    @Column(name = "aula")
     private String aula;
-
-    @Column(name = "cupo_maximo")
     private int cupoMaximo;
-
-    @Column(name = "periodo_evaluacion")
     private String periodoEvaluacion;
-    
+
 
     public Long getId() {
         return id;
