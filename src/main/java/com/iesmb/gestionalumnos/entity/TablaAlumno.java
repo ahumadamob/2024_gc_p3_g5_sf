2464b180546baaafeb3b53e17fc23b603534284f
Valeria Grassi <valeegrassi@gmail.com>
2024-05-09 21:04:09 -0300

    Primer corrección entidad Alumno

--- src/main/java/com/iesmb/gestionalumnos/entity/TablaAlumno.java
+++ src/main/java/com/iesmb/gestionalumnos/entity/TablaAlumno.java
@@ -17,38 +17,28 @@ public class TablaAlumno {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
 	private Integer id;
 	@NotBlank(message = "El nombre no puede estar vacío.")
 	@Size(max = 30, message = "El nombre no debe superar los 30 caracteres.")
-	@Column(name="nombre")
 	private String nombre;
 	@NotBlank(message = "El apellido no puede estar vacío.")
 	@Size(max = 30, message = "El apellido no debe superar los 30 caracteres.")
-	@Column(name="apellido")
 	private String apellido;
 	@NotNull(message = "La fecha de nacimiento no pueden estar vacía.")
 	@Future(message = "La fecha de nacimiento es incorrecta.")
-	@Column(name="fechaNacimiento")
 	private Date fechaNacimiento;
 	@NotBlank(message = "Debe indicar un correo electrónico.")
-	@Column(name="correoElectronico")
 	private String correoElectronico;
     @NotBlank(message = "La dirección no puede estar vacía.")
 	@Size(max = 60, message = "La dirección no debe superar los 60 caracteres.")
-    @Column(name="direccion")
     private String direccion;
     @NotBlank(message = "Debe ingresar un número de teléfono.")
 	@Size(max = 10, message = "El teléfono no debe superar los 10 caracteres.")
-    @Column(name="numeroTelefono")
     private String numeroTelefono;
-    @Column(name="genero")
     private String genero;
     @NotNull(message = "La fecha de inscripción no pueden estar vacía.")
 	@Future(message = "La fecha de inscripción es incorrecta.")
-    @Column(name="fechaInscripcion")
     private Date fechaInscripcion;
-    @Column(name="estado")
     private String estado;
     
 	public Integer getId() {
