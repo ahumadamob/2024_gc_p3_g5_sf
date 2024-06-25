7c01750b99247d70c840994e3f650c40c66a40f1
Ramiro Garro <ramagarro@gmail.com>
2024-06-25 17:08:03 -0300

    Persistencia entidad Profesor

---
+++ src/main/java/com/iesmb/gestionalumnos/service/IProfesorService.java
@@ -0,0 +1,15 @@
+package com.iesmb.gestionalumnos.service;
+
+import java.util.List;
+
+import com.iesmb.gestionalumnos.entity.Profesor;
+
+public interface IProfesorService {
+
+	public List<Profesor> getAll ();
+	public Profesor getById (Integer id);
+	public Profesor save (Profesor turno);
+	public void delete (Integer id);
+	public boolean exists (Integer id);
+	
+}
---
+++ src/main/java/com/iesmb/gestionalumnos/service/jpa/ProfesorServiceImpl.java
@@ -0,0 +1,45 @@
+package com.iesmb.gestionalumnos.service.jpa;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.iesmb.gestionalumnos.entity.Profesor;
+import com.iesmb.gestionalumnos.repository.ProfesorRepository;
+import com.iesmb.gestionalumnos.service.IProfesorService;
+
+@Service
+@Primary
+public class ProfesorServiceImpl implements IProfesorService{
+
+	@Autowired
+	ProfesorRepository repo;
+
+	@Override
+	public List<Profesor> getAll() {		
+		return repo.findAll();
+	}
+	
+	@Override
+	public Profesor getById(Integer id) {
+        return repo.findById(id).orElse(null);
+    }	
+
+	@Override
+	public Profesor save(Profesor turno) {
+		return repo.save(turno);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		repo.deleteById(id);		
+	}
+
+	@Override
+	public boolean exists(Integer id) {
+	    return (id == null) ? false: repo.existsById(id);
+	}
+	
+}
