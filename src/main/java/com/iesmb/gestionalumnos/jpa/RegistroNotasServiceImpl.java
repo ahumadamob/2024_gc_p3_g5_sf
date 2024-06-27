81c4e0e781aba25e1a7252ccdc06819a3ca69648
Feduuz <federico.mbv@gmail.com>
2024-06-27 02:06:04 -0300

    Nuevos paquetes y controladores.

---
+++ src/main/java/com/iesmb/gestionalumnos/jpa/RegistroNotasServiceImpl.java
@@ -0,0 +1,49 @@
+package com.iesmb.gestionalumnos.jpa;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.iesmb.gestionalumnos.entity.RegistroNotas;
+import com.iesmb.gestionalumnos.repository.RegistroNotasRepository;
+import com.iesmb.gestionalumnos.service.IRegistroNotasService;
+
+@Service
+@Primary
+public class RegistroNotasServiceImpl implements IRegistroNotasService {
+
+	@Autowired
+	RegistroNotasRepository repo;
+	
+	@Override
+	public List<RegistroNotas> getAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public RegistroNotas getById(Integer id) {
+		Optional<RegistroNotas> optional = repo.findById(id);
+		return optional.orElse(null);
+	}
+
+	@Override
+	public RegistroNotas save(RegistroNotas registroNotas) {
+		return repo.save(registroNotas);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		repo.deleteById(id);
+	}
+
+	@Override
+	public boolean exists(Integer id) {
+		return (id == null)? false: repo.existsById(id);
+	}
+	
+	
+	
+}
