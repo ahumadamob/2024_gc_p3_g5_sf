6efa0603acc9e72e5d0d315a6434d5d474de4eb5
Ivan Balmaceda <balmacedaivan5@gmail.com>
2024-09-26 13:05:51 -0300

    AuthController en controladores

---
+++ src/main/java/com/iesmb/gestionalumnos/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.iesmb.gestionalumnos.controller;
+
+import com.iesmb.gestionalumnos.dto.JwtResponse;
+import com.iesmb.gestionalumnos.dto.LoginRequest;
+import com.iesmb.gestionalumnos.security.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	
+	@Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+
+}
